e32d5626003955c9160a426f5995946264a5b3b2
cybernezo <aurenoah12@gmail.com>
2026-03-06 20:50:15 +0100

    Create fsediufez!fveazu!çvrjeuçvrthV375Y379vy37V5Y37V5NT35V673V5TN3V65V365V36.lua

---
+++ fsediufez!fveazu!çvrjeuçvrthV375Y379vy37V5Y37V5NT35V673V5TN3V65V365V36.lua
@@ -0,0 +1,33 @@
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+
+local ALLOWED_USERIDS = {10386158945, 2370015588}
+local player = Players.LocalPlayer
+
+local function notify(title, text, duration)
+    StarterGui:SetCore("SendNotification", {
+        Title = title;
+        Text = text;
+        Duration = duration or 5;
+    })
+end
+
+-- Vérifie si l'utilisateur est autorisé
+local function isAllowed(id)
+    for _, v in pairs(ALLOWED_USERIDS) do
+        if v == id then
+            return true
+        end
+    end
+    return false
+end
+
+if isAllowed(player.UserId) then
+    notify("Private Nezo", "Valide User", 5)
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/cybernezo/privatenezo/main/private.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/cybernezo/privatenezo/main/tpmenu.lua"))()
+
+else
+    notify("Private Nezo", "Invalide User", 5)
+end
